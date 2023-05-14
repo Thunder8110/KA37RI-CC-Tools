0e8116b40d2a5170e8b68ff56abed5d30c6d466b
Thunder8110 <44309219+Thunder8110@users.noreply.github.com>
2023-05-14 09:09:49 +0900

    Update kccas.js
    used object "kcct"

--- kccas.js
+++ kccas.js
@@ -1,7 +1,11 @@
-let dirHandle = await window.showDirectoryPicker();
-let intervalId = setInterval(async () => {
-  let fileHandle = await dirHandle.getFileHandle(Date.now() + ".txt", { create: true });
-  let writable = await fileHandle.createWritable();
-  await writable.write(Game.WriteSave(1));
-  writable.close();
+if (typeof kcct !== "undefined") {
+  let kcct = {}
+}
+
+let kcct["dirHandle"] = await window.showDirectoryPicker();
+let kcct["intervalId"] = setInterval(async () => {
+  let kcct["fileHandle"] = await dirHandle.getFileHandle(Date.now() + ".txt", { create: true });
+  let kcct["writable"] = await fileHandle.createWritable();
+  await kcct["writable"].write(Game.WriteSave(1));
+  kcct["writable"].close();
   }, 60 * 60 * 1000);
