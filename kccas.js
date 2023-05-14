75614622e21aa3305f2a6e19f4000fbd20acb778
Thunder8110 <44309219+Thunder8110@users.noreply.github.com>
2023-05-14 10:22:08 +0900

    Update kccas.js
    removed querying permission

--- kccas.js
+++ kccas.js
@@ -5,7 +5,6 @@ if (typeof kcct === "undefined") {
 kcct["dirHandle"] = await window.showDirectoryPicker();
 kcct["dirHandle"].requestPermission({mode: "readwrite"});
 kcct["intervalId"] = setInterval(async () => {
-  let permission = await kcct["dirHandle"].queryPermission({mode: "readwrite"});
   kcct["fileHandle"] = await kcct["dirHandle"].getFileHandle(Date.now() + ".txt", {create: true});
   kcct["writable"] = await kcct["fileHandle"].createWritable();
   await kcct["writable"].write(Game.WriteSave(1));
